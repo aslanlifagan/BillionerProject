184683c373308d5c424b637fd67338a493d50e10
aslanlifagan <85606751+aslanlifagan@users.noreply.github.com>
2024-10-05 13:43:26 +0400

    integrated connection with controller

--- BillionerProject/Auth/Controllers/GetStartedController.swift
+++ BillionerProject/Auth/Controllers/GetStartedController.swift
@@ -15,11 +15,24 @@ final class GetStartedController: UIViewController {
     
     fileprivate func configureButton() {
         submitButton.addTarget(self, action: #selector(submitButtonClicked), for: .touchUpInside)
+        submitButton.setTitle("Get Started", for: .normal)
     }
     
+    fileprivate func configureUI() {
+        
+    }
     @objc
     fileprivate func submitButtonClicked() {
-        print(#function)
+        showRegisterController()
+    }
+    
+    fileprivate func showRegisterController() {
+        let vc = UIStoryboard
+            .init(
+                name: "Auth",
+                bundle: nil
+            ).instantiateViewController(withIdentifier: "RegisterController") as? RegisterController ?? RegisterController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
--- BillionerProject/Auth/Controllers/RegisterController.swift
+++ BillionerProject/Auth/Controllers/RegisterController.swift
@@ -8,18 +8,34 @@
 import UIKit
 final class RegisterController: UIViewController {
     
-    @IBOutlet private weak var submitButton: UIButton!
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var nameField: UITextField!
+    @IBOutlet private weak var surnameField: UITextField!
+    @IBOutlet private weak var passwordField: UITextField!
+    @IBOutlet private weak var mailField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    fileprivate func configureUI() {
+        navigationController?.setNavigationBarHidden(true, animated: true)
         configureButton()
     }
     
     fileprivate func configureButton() {
-        submitButton.addTarget(self, action: #selector(submitButtonClicked), for: .touchUpInside)
+        signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+        signUpButton.setTitle("Sign Up", for: .normal)
+        loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
+        loginButton.setTitle("Login", for: .normal)
     }
     
     @objc
-    fileprivate func submitButtonClicked() {
+    fileprivate func signUpButtonClicked() {
+        print(#function)
+    }
+    @objc
+    fileprivate func loginButtonClicked() {
         print(#function)
     }
 }
