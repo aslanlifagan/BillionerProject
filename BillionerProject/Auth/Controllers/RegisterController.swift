473ac0f692897edb34321ed1e304c7ab7cfb0df1
aslanlifagan <85606751+aslanlifagan@users.noreply.github.com>
2024-10-05 13:31:26 +0400

    Created RegisterController
    finished UI companent

---
+++ BillionerProject/Auth/Controllers/RegisterController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterController.swift
+//  BillionerProject
+//
+//  Created by Aslanli Faqan on 05.10.24.
+//
+
+import UIKit
+final class RegisterController: UIViewController {
+    
+    @IBOutlet private weak var submitButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureButton()
+    }
+    
+    fileprivate func configureButton() {
+        submitButton.addTarget(self, action: #selector(submitButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc
+    fileprivate func submitButtonClicked() {
+        print(#function)
+    }
+}
+
