b2bb6e2ab9ce4a9864283b34aa63660b06ca456c
aslanlifagan <85606751+aslanlifagan@users.noreply.github.com>
2024-10-05 12:46:21 +0400

    Initial commit

---
+++ BillionerProject/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  BillionerProject
+//
+//  Created by Aslanli Faqan on 05.10.24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# BillionerProject
+ 
